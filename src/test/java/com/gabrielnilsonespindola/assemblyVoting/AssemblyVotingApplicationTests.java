fbab444a5049c05c72c99e961460bd0083a2e4f7
Gabriel Nilson Espindola <gabriel.ne@hotmail.com>
2025-05-10 22:37:25 -0300

    primeiro commit , projeto criado

---
+++ src/test/java/com/gabrielnilsonespindola/assemblyVoting/AssemblyVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielnilsonespindola.assemblyVoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssemblyVotingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
