7c4b4a1b2e89a116804883c501a0e419d60b44f2
Gabriel Nilson Espindola <gabriel.ne@hotmail.com>
2025-05-12 15:50:50 -0300

    correção mongodb-postmann, conexao bem sucedida

--- src/main/java/com/gabrielnilsonespindola/assemblyVoting/resources/UserResource.java
+++ src/main/java/com/gabrielnilsonespindola/assemblyVoting/resources/UserResource.java
@@ -1,28 +1,28 @@
 package com.gabrielnilsonespindola.assemblyVoting.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gabrielnilsonespindola.assemblyVoting.domain.User;
+import com.gabrielnilsonespindola.assemblyVoting.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
+	@Autowired
+	private UserService service;
+
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll() {
-		User maria = new User("1" , "Maria" , "SIM");
-		User gabriel = new User("2" , "gabriel" ,"NAO");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria,gabriel));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
-		
+
 	}
 
 }
--- src/main/java/com/gabrielnilsonespindola/assemblyVoting/services/UserService.java
+++ src/main/java/com/gabrielnilsonespindola/assemblyVoting/services/UserService.java
@@ -1,6 +1,23 @@
 package com.gabrielnilsonespindola.assemblyVoting.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrielnilsonespindola.assemblyVoting.domain.User;
+import com.gabrielnilsonespindola.assemblyVoting.repository.UserRepository;
+
+@Service
 public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	
+	public List <User> findAll () {
+		return repo.findAll();	
+	}
 	
 	
 
